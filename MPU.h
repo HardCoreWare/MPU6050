07dd18f4f5e5081b2ada2bb6192815ac06dca939
Bernardo Colorado Dubois <33713253+HardCoreWare@users.noreply.github.com>
2018-04-13 11:13:44 -0500

    Update MPU.h

--- MPU.h
+++ MPU.h
@@ -1,18 +1,8 @@
 #ifndef _MPU_h_
 #define _MPU_h_
 
-
-/*
-This is my first library and is and debugging and upgrade of code developed by Joop Brokking
-Website: http://www.brokking.net/imu.html
-Youtube: https://youtu.be/4BoIE8YQwM8
-Version: 1.0 (May 5, 2016)
-// Please support suscribing to his channel and website
-*/
-
 #include <inttypes.h>
 #include <Wire.h>
-
 
 class MPU{
 
@@ -37,19 +27,17 @@ double angle[3];
 
 }acc;
 
+//gyroscope variables
 struct Gyro{
-  
 long cal[3];
 double spin[3];
 double angle[3];
-
 }gyro;
 
-struct Output{
-	
+//output data for getters
+struct Output{	
 double angle[3];
 double radial[3];       // not used yet
-
 }output;
 
 long temp;
